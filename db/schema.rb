536525a0aa7a7b5a389d504ac90507543e455072
Freon26 <xexo26@yahoo.com.ph>
2013-12-02 21:26:30 -0800

    Added social 6

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203044639) do
+ActiveRecord::Schema.define(version: 20131203052327) do
 
   create_table "spree_activators", force: true do |t|
     t.string   "description"
